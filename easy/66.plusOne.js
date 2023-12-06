30c91194268675ece8e670cba6750453b5a5504b
Zachary Theodore <zacharytheodore.zac@gmail.com>
2023-12-06 11:19:06 +0700

    plusOne

--- easy/66.plusOne.js
+++ easy/66.plusOne.js
@@ -1,7 +1,16 @@
-let digits = [1, 2, 3];
-//Output: [1,2,4]
+let digits = [9, 9];
+
 var plusOne = function (digits) {
-    
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] == 9) {
+      digits[i] = 0;
+    } else {
+      digits[i]++;
+      return digits;
+    }
+  }
+  digits.unshift(1);
+  return digits;
 };
 
 console.log(plusOne(digits));
