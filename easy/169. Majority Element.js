c5334d70daae5ec64a3ffab62852ce9b86dbceff
Zachary Theodore <zacharytheodore.zac@gmail.com>
2023-12-07 09:48:25 +0700

    169

---
+++ easy/169. Majority Element.js
@@ -0,0 +1,16 @@
+let nums = [2, 2, 1, 1, 1, 2, 2];
+
+var majorityElement = function (nums) {
+  let candidate;
+  let count = 0;
+
+  for (let num of nums) {
+    if (count == 0) {
+      candidate = num;
+    }
+    count += (num == candidate) ? +1 : -1
+  }
+  return candidate
+};
+
+console.log(majorityElement(nums));
