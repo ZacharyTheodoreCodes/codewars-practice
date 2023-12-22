89f8927791a79aac3633ef536e4ca7c6389024e2
Zachary Theodore <zacharytheodore.zac@gmail.com>
2023-12-22 19:56:09 +0700

    70

--- easy/70. Climbing Stairs.js
+++
@@ -1,9 +0,0 @@
-var climbStairs = function (n) {
-  let arr = [1, 1];
-  for (let i = 2; i <= n; i++) {
-    arr.push(arr[i - 1] + arr[i - 2]);
-  }
-  return arr[n];
-};
-
-console.log(climbStairs(5));
