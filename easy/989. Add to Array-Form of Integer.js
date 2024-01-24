9fcbad148fb649e59d845bf64ee8eafe688fd44d
Zachary Theodore <zacharytheodore.zac@gmail.com>
2024-01-24 13:46:01 +0700

    989

--- easy/989. Add to Array-Form of Integer.js
+++ easy/989. Add to Array-Form of Integer.js
@@ -1,7 +1,13 @@
 var addToArrayForm = function (num, k) {
-    
+  let sum = BigInt(num.join("")) + BigInt(k);
+  return sum
+    .toString()
+    .split("")
+    .map((x) => Number(x));
 };
 
-let num = [1, 2, 0, 0];
+let num = [
+  1, 2, 0, 0, 1, 2, 0, 0, 1, 2, 0, 0, 1, 2, 0, 0, 1, 2, 0, 0, 1, 2, 0, 0,
+];
 let k = 34;
 console.log(addToArrayForm(num, k));
