8c398f6221b41323538494590566d94eab00b909
Zachary Theodore <zacharytheodore.zac@gmail.com>
2024-01-25 14:29:45 +0700

    linkedlist 2

--- easy/linkedList2.js
+++ easy/linkedList2.js
@@ -39,14 +39,66 @@ class LinkedList {
       return;
     }
 
-   let count = 0 ;
-    
+    let count = 0;
+    let current = this.head;
+    let previous;
+    let newNode = new Node(data);
+    while (count < index) {
+      count++;
+      previous = current;
+      current = current.next;
+    }
+
+    newNode.next = current;
+    previous.next = newNode;
+    this.size++;
+  }
+
+  getAt(index) {
+    let current = this.head;
+    let count = 0;
+    while (count < index) {
+      count++;
+      current = current.next;
+    }
+    console.log(current);
+    return;
+  }
+
+  removeAt(index) {
+    if (index > 0 && index > this.size) {
+      return;
+    }
+    let count = 0;
+    let current = this.head;
+    let previous;
+
+    if (index == 0) {
+      this.head = current.next;
+    } else {
+      while (count < index) {
+        count++;
+        previous = current;
+        current = current.next;
+      }
+      previous.next = current.next;
+    }
+
+    this.size--;
+  }
+
+  clearList() {
+    this.head = null;
+    this.size = 0;
   }
 }
 
 let a = new Node(1, 0);
 let ll = new LinkedList();
-ll.insertFirst(3);
 ll.insertFirst(2);
+ll.insertFirst(1);
 ll.insertLast(4);
+ll.insertAt(3, 2);
+ll.getAt(1);
+//ll.removeAt(2);
 console.dir(ll, { depth: null });
